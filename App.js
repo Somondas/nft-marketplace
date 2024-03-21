81e3cbe066dd27dbf873d6716466df8c175b067d
Somondas <somondas067@gmail.com>
2024-03-21 23:35:21 +0530

    Updated

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./screens/Home";
+import Details from "./screens/Details";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
-  return <NavigationContainer></NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Home"
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
