8c2b6ae2c58751d8ddd2d3b7fb5c48a5de6f80e7
Somondas <somondas067@gmail.com>
2024-03-23 20:15:41 +0530

    Working on DetailsDesc

--- components/DetailsDesc.js
+++ components/DetailsDesc.js
@@ -1,17 +1,54 @@
 import React from "react";
 import { EthPrice, NFTtitle } from "./SubInfo";
-import { SIZES } from "../constants";
+import { COLORS, FONTS, SIZES } from "../constants";
+import { Text, View } from "react-native";
 
 export default function DetailsDesc({ data }) {
   return (
-    <View>
-      <NFTtitle
-        title={data.name}
-        subTitle={data.creator}
-        titleSize={SIZES.extraLarge}
-        subTitleSize={SIZES.font}
-      />
-      <EthPrice price={data.price} />
-    </View>
+    <>
+      <View
+        style={{
+          width: "100%",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <NFTtitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.extraLarge}
+          subTitleSize={SIZES.font}
+        />
+        <EthPrice price={data.price} />
+      </View>
+      <View
+        style={{
+          marginVertical: SIZES.extraLarge * 1.5,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: SIZES.font,
+            fontFamily: FONTS.semiBold,
+            color: COLORS.primary,
+          }}
+        >
+          Description
+        </Text>
+        <View style={{ marginTop: SIZES.base }}>
+          <Text
+            style={{
+              fontSize: SIZES.small,
+              fontFamily: FONTS.regular,
+              color: COLORS.secondary,
+              lineHeight: SIZES.large,
+            }}
+          >
+            {data.description}
+          </Text>
+        </View>
+      </View>
+    </>
   );
 }
