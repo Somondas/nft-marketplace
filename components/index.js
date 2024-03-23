6a9c7f350d2a9ab1796f761bf22179378f1070c2
Somondas <somondas067@gmail.com>
2024-03-23 18:30:51 +0530

    Working on Details Page

--- components/index.js
+++ components/index.js
@@ -1,6 +1,16 @@
 import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
 import FocusStatusBar from "./FocusStatusBar";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
 import { CircleButton, RectButton } from "./Button";
 
-export { HomeHeader, NFTCard, FocusStatusBar, CircleButton, RectButton };
+export {
+  HomeHeader,
+  NFTCard,
+  FocusStatusBar,
+  CircleButton,
+  RectButton,
+  DetailsBid,
+  DetailsDesc,
+};
--- screens/Details.js
+++ screens/Details.js
@@ -1,11 +1,44 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
+import {
+  CircleButton,
+  RectButton,
+  SubInfo,
+  FocusStatusBar,
+  DetailsDesc,
+  DetailsBid,
+} from "../components";
+import { COLORS, SIZES, SHADOWS, FONTS, assets } from "../constants";
+import { FlatList } from "react-native-gesture-handler";
 
-const Details = () => {
+const Details = ({ route, navigation }) => {
+  const { data } = route.params;
+
   return (
-    <View>
-      <Text>Details</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+      <View
+        style={{
+          width: "100%",
+          position: "absolute",
+          bottom: 0,
+          paddingVertical: SIZES.font,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(255, 255, 255, .5)",
+          zIndex: 1,
+        }}
+      >
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS} />
+      </View>
+      <FlatList
+      // data={}
+      />
+    </SafeAreaView>
   );
 };
 
